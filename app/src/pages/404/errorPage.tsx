4e31ac8b4e46f1abd219cdb75ed216d5fa820161
petro1909 <petya-97_97@mail.ru>
2023-03-10 11:40:26 +0300

    feat: add error page component

--- app/src/pages/404/errorPage.tsx
+++ app/src/pages/404/errorPage.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import './errorPage.css';
 
 export default class ErrorPage extends React.Component {
   render() {
-    return <section className="error">Ooooops! Something went wrong</section>;
+    return <section className="error-wrapper">Ooooops! Something went wrong</section>;
   }
 }
