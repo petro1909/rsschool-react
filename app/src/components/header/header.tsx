6051432f6990c2035a1188cce605334a9a5e30dd
petro1909 <petya-97_97@mail.ru>
2023-03-10 11:23:40 +0300

    feat: create header component

--- app/src/components/header/header.tsx
+++ app/src/components/header/header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './header.css';
 
 export default class Header extends React.Component {
   render() {
     return (
-      <header>
+      <header className="header">
         <nav>
           <ul>
             <li>
