24ebf8899a6bd7b4d4c29a876251597ee68bb74e
petro1909 <petya-97_97@mail.ru>
2023-03-10 11:25:11 +0300

    feat: create cards component

---
+++ app/src/components/cards/cards.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import getItemList from '../../service/fetchService';
+import { Product, ProductJsonResult } from '../../types/product';
+import Card from '../card/card';
+import './cards.css';
+type Props = {
+  value?: string;
+};
+type CardsState = {
+  cards: Product[];
+};
+
+export default class Cards extends React.Component<Props, CardsState> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { cards: [] };
+  }
+  async componentDidMount(): Promise<void> {
+    const fetchedProducts = (await getItemList(
+      'https://dummyjson.com/products?limit=20'
+    )) as ProductJsonResult | null;
+    if (!fetchedProducts) {
+      this.setState({ cards: [] });
+    } else {
+      this.setState({ cards: fetchedProducts.products });
+    }
+  }
+
+  render() {
+    return (
+      <section className="cards-wrapper">
+        {this.state.cards.map((product) => {
+          return <Card key={product.id} item={product} />;
+        })}
+      </section>
+    );
+  }
+}
