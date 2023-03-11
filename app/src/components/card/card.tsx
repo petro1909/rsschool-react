92a7382825f83b3fe1037966f51d0c59ce979e67
petro1909 <petya-97_97@mail.ru>
2023-03-11 21:41:57 +0300

    refactor: move element declaration to one line

--- app/src/components/card/card.tsx
+++ app/src/components/card/card.tsx
@@ -14,10 +14,7 @@ export default class Card extends React.Component<Props> {
     return (
       <section className="product-wrapper">
         <div className="product">
-          <div
-            className="product__picture-wrapper"
-            style={{ backgroundImage: `url("${this.props.item.thumbnail}")` }}
-          ></div>
+          <div className="product__picture-wrapper" style={{ backgroundImage: `url("${this.props.item.thumbnail}")` }}></div>
           <div className="product__description">
             <h3 className="product__title">{this.props.item.title}</h3>
             <p className="product__brand">{this.props.item.brand}</p>
